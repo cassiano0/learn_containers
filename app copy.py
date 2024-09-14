66445d181d58e5d1e06abf96eb6d0ce0cfd3f037
cassiano0 <cassiano0@users.noreply.github.com>
2024-09-14 19:30:12 -0300

    primeiros containers com db postegresql e python
    arquivos de testes iniciais. Com network isolando banco de dados e proxy controlando acesso a varios sites.

---
+++ app copy.py
@@ -0,0 +1,26 @@
+import psycopg2
+from flask import Flask
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host='db',  # Este é o nome do serviço do PostgreSQL no docker-compose
+        database='meu_app',
+        user='user',
+        password='password'
+    )
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT 1')  # Apenas um teste para verificar a conexão
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"Conexão com o banco de dados bem-sucedida site 2!! {result}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
